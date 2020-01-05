ba821784a861859a404c39cb810c57f3e7b8701a
Mohammed Alateyya <mnur@cmu.edu>
2020-01-05 11:35:43 +0300

    update browser wasm

--- matmul.rs
+++ matmul.rs
@@ -1,50 +1,45 @@
 #![no_std]
 use core::panic::PanicInfo;
-use core::mem::MaybeUninit;
-use core::slice::{
-    from_raw_parts,
-    from_raw_parts_mut,
-};
 
-#[no_mangle]
-pub fn add_one(x: i32) -> i32 {
-    x + 1
+extern "C" {
+    fn now() -> f64;
+    fn alert(s: &str);
 }
 
+static A: [i16; 250000] = [1; 250000];
+static B: [i16; 250000] = [1; 250000];
+static mut C: [i16; 250000] = [0; 250000];
+
 #[no_mangle]
-pub fn matmul(
-    n: i32, m: i32, k:i32,
-    a: *const i32, b: *const i32, c: *mut i32
-) -> i32 {
-    let (n, m, k) = (n as usize, m as usize, k as usize);
+pub fn matmul(size: i32) -> f64 {
+    let a = &A[0] as *const i16;
+    let b = &B[0] as *const i16;
+    let c = unsafe { &mut C[0] as *mut i16 };
+    let size = size as isize;
     unsafe {
-        let a = from_raw_parts(a, n*m);
-        let b = from_raw_parts(b, m*k);
-        let c = from_raw_parts_mut(c, n*k);
-        for _ in 0..300 {
-            for i in 0..n {
-                for j in 0..k {
+        let start = now();
+        for _ in 0..5 {
+            for i in 0..size {
+                for j in 0..size {
                     let mut acc = 0;
-                    for l in 0..m {
-                        acc += a[i*m+l] * b[l*k+j]
+                    for l in 0..size {
+                        acc += a.offset(i*size+l).read() * b.offset(l*size+j).read();
                     }
-                    c[i*k+j] = acc;
+                    c.offset(i*size+j).write(acc);
                 }
             }
         }
-        return c[0];
+        let end = now();
+        for i in 0..size {
+            assert_eq!(c.offset(i*size + i).read(), size as i16);
+        }
+        end - start
     }
 }
 
 #[no_mangle]
-pub fn main() {
-    let a: [i32; 10000] = [1; 10000];
-    let b: [i32; 10000] = [1; 10000];
-    let mut c: [i32; 10000] = unsafe { MaybeUninit::uninit().assume_init() };
-    matmul(100, 100, 100, &a[0] as *const i32, &b[0] as *const i32, &mut c[0] as *mut i32);
-}
-
-
-#[no_mangle]
 #[panic_handler]
-pub fn panic(_info: &PanicInfo) -> ! { loop {} }
+pub fn panic(_info: &PanicInfo) -> ! {
+    unsafe { alert("Panic!") };
+    loop {}
+}
